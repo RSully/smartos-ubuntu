b24ef24a289bb3b1e92af5c3cf319a2a9b7dff24
Ryan <rsul.dev@me.com>
2015-01-13 07:22:31 -0500

    Test resize command with smaller zpool

--- 1-prep.sh
+++ 1-prep.sh
@@ -4,4 +4,8 @@ if [ ! -f ubuntu-14.04-server-cloudimg-amd64-disk1.img ]; then
 	wget 'https://cloud-images.ubuntu.com/releases/14.04.1/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img'
 fi
 
+# Convert compressed image to raw image
 qemu-img convert -O raw ubuntu-14.04-server-cloudimg-amd64-disk1.img ubuntu-14.04-server-cloudimg-amd64-disk1.dist.img
+
+# Convert raw image to exactly 2GB
+qemu-img resize ubuntu-14.04-server-cloudimg-amd64-disk1.dist.img 2G
--- 2-run.sh
+++ 2-run.sh
@@ -4,9 +4,9 @@
 sudo mount-image-callback ubuntu-14.04-server-cloudimg-amd64-disk1.dist.img -- sh -c 'sed -i "s/, None/, SmartOS, None/" $MOUNTPOINT/etc/cloud/cloud.cfg.d/90_dpkg.cfg'
 
 # Create a ZFS volume to write the image to
-sudo fallocate -l 10G /myzpool.file
+sudo fallocate -l 5G /myzpool.file
 sudo zpool create myzpool /myzpool.file
-sudo zfs create -V 5G myzpool/ubuntu
+sudo zfs create -V 2G myzpool/ubuntu
 
 # Write the image and store it as a gzipped ZFS volume
 sudo dd if=ubuntu-14.04-server-cloudimg-amd64-disk1.dist.img of=/dev/zvol/myzpool/ubuntu
